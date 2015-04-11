a6deff0c4a913fe3cf6e33a623b816a57a209da7
Jason Curl <jcurl@arcor.de>
2015-04-11 12:57:29 +0000

    Updated comment headers and properties.

--- code/Native/UnsafeNativeMethods.cs
+++ code/Native/UnsafeNativeMethods.cs
@@ -1,5 +1,8 @@
-﻿// $URL: $
-// $Id: $
+﻿// $URL$
+// $Id$
+
+// Copyright © Jason Curl 2012-2014.
+// See http://serialportstream.codeplex.com for license details (MS-PL License)
 
 namespace RJCP.IO.Ports.Native
 {
