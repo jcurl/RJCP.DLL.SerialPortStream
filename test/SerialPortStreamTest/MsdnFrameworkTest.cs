7f381f336f9dc07b1786b3596063e1654e3f459c
Jason Curl <jcurl@arcor.de>
2016-04-17 18:49:37 +0200

    Add test cases on what happens when write is blocked and we close/dispose.

--- test/SerialPortStreamTest/MsdnFrameworkTest.cs
+++ test/SerialPortStreamTest/MsdnFrameworkTest.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO.Ports;
+    using System.Threading;
     using NUnit.Framework;
 
     [TestFixture]
@@ -23,6 +24,74 @@
 
             using (SerialPort serialSource = new SerialPort(c_SourcePort, 115200, Parity.None, 8, StopBits.One)) {
                 Assert.That(() => { serialSource.Write(buffer, 0, buffer.Length); }, Throws.TypeOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        [Timeout(4000)]
+        public void SerialPortDisposedWhenBlocked()
+        {
+            byte[] buffer = new byte[1024];
+
+            using (SerialPort serialSource = new SerialPort(c_SourcePort, 115200, Parity.None, 8, StopBits.One))
+            using (SerialPort serialDest = new SerialPort(c_DestPort, 115200, Parity.None, 8, StopBits.One)) {
+                serialSource.Open();
+                serialDest.Open();
+
+                serialDest.RtsEnable = false;
+
+                new Thread(
+                    () => {
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Disposing serialSource");
+
+                        // It appears that the MSDN .NET implementation blocks here, never
+                        // to return as we're blocked on another thread.
+                        serialSource.Dispose();
+                        Console.WriteLine("Disposed serialSource");
+                    }
+                ).Start();
+
+                int bufferCount = 1024 * 1024;
+                while (bufferCount > 0) {
+                    serialSource.Write(buffer, 0, buffer.Length);
+                    bufferCount -= buffer.Length;
+                    Console.WriteLine("{0}", bufferCount);
+                }
+            }
+        }
+
+        [Test]
+        [Timeout(4000)]
+        public void SerialPortClosedWhenBlocked()
+        {
+            byte[] buffer = new byte[1024];
+
+            using (SerialPort serialSource = new SerialPort(c_SourcePort, 115200, Parity.None, 8, StopBits.One))
+            using (SerialPort serialDest = new SerialPort(c_DestPort, 115200, Parity.None, 8, StopBits.One)) {
+                serialSource.Open();
+                serialDest.Open();
+
+                serialDest.RtsEnable = false;
+
+                new Thread(
+                    () => {
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Closing serialSource");
+
+                        // It appears that the MSDN .NET implementation blocks here, never
+                        // to return as we're blocked on another thread.
+                        serialSource.Close();
+                        Console.WriteLine("Closed serialSource");
+                    }
+                ).Start();
+
+                int bufferCount = 1024 * 1024;
+                while (bufferCount > 0) {
+                    serialSource.Write(buffer, 0, buffer.Length);
+                    bufferCount -= buffer.Length;
+                    Console.WriteLine("{0}", bufferCount);
+                }
             }
         }
     }
--- test/SerialPortStreamTest/SerialPortStreamTest.cs
+++ test/SerialPortStreamTest/SerialPortStreamTest.cs
@@ -1414,5 +1414,89 @@ namespace RJCP.IO.Ports.SerialPortStreamTest
                 }, Throws.TypeOf<InvalidOperationException>());
             }
         }
+
+        [Test]
+        [Category("SerialPortStream")]
+        [Timeout(4000)]
+        public void DisposedWhenBlocked()
+        {
+            byte[] buffer = new byte[1024];
+
+            using (SerialPortStream serialSource = new SerialPortStream(c_SourcePort, 115200, 8, Parity.None, StopBits.One))
+            using (SerialPortStream serialDest = new SerialPortStream(c_DestPort, 115200, 8, Parity.None, StopBits.One)) {
+                serialSource.ReadBufferSize = 8192;
+                serialSource.WriteBufferSize = 8192;
+                serialDest.ReadBufferSize = 8192;
+                serialDest.WriteBufferSize = 8192;
+                serialSource.Open();
+                serialDest.Open();
+
+                serialDest.RtsEnable = false;
+
+                new Thread(
+                    () => {
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Disposing serialSource");
+
+                        // It appears that the MSDN .NET implementation blocks here, never
+                        // to return as we're blocked on another thread.
+                        serialSource.Dispose();
+                        Console.WriteLine("Disposed serialSource");
+                    }
+                ).Start();
+
+                Assert.That(
+                    () => {
+                        int bufferCount = 1024 * 1024;
+                        while (bufferCount > 0) {
+                            serialSource.Write(buffer, 0, buffer.Length);
+                            bufferCount -= buffer.Length;
+                            Console.WriteLine("{0}", bufferCount);
+                        }
+                    }, Throws.InstanceOf<ObjectDisposedException>());
+            }
+        }
+
+        [Test]
+        [Category("SerialPortStream")]
+        [Timeout(4000)]
+        public void ClosedWhenBlocked()
+        {
+            byte[] buffer = new byte[1024];
+
+            using (SerialPortStream serialSource = new SerialPortStream(c_SourcePort, 115200, 8, Parity.None, StopBits.One))
+            using (SerialPortStream serialDest = new SerialPortStream(c_DestPort, 115200, 8, Parity.None, StopBits.One)) {
+                serialSource.ReadBufferSize = 8192;
+                serialSource.WriteBufferSize = 8192;
+                serialDest.ReadBufferSize = 8192;
+                serialDest.WriteBufferSize = 8192;
+                serialSource.Open();
+                serialDest.Open();
+
+                serialDest.RtsEnable = false;
+
+                new Thread(
+                    () => {
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Closing serialSource");
+
+                        // It appears that the MSDN .NET implementation blocks here, never
+                        // to return as we're blocked on another thread.
+                        serialSource.Close();
+                        Console.WriteLine("Closed serialSource");
+                    }
+                ).Start();
+
+                Assert.That(
+                    () => {
+                        int bufferCount = 1024 * 1024;
+                        while (bufferCount > 0) {
+                            serialSource.Write(buffer, 0, buffer.Length);
+                            bufferCount -= buffer.Length;
+                            Console.WriteLine("{0}", bufferCount);
+                        }
+                    }, Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
     }
 }
