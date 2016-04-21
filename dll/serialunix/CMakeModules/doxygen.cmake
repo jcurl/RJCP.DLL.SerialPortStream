224463c52f4488d9d95b051ab6592d81037974d1
Jason Curl <jcurl@arcor.de>
2016-04-21 20:54:57 +0200

    Slightly change formatting

--- dll/serialunix/CMakeModules/doxygen.cmake
+++ dll/serialunix/CMakeModules/doxygen.cmake
@@ -6,7 +6,7 @@
 # This module looks for Doxygen and sets up targets in your build system to build documentation.
 #
 #  set (CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/CMakeModules)
-#  include (${CMAKE_MODULE_PATH}/doxygen.cmake)  
+#  include (${CMAKE_MODULE_PATH}/doxygen.cmake)
 #
 # The doxygen input file must be in the source directory where the CMakeLists.txt file is and be
 # called 'doxyfile.in'. That will be converted to 'doxyfile'.
@@ -19,14 +19,14 @@
 find_package(Doxygen)
 option(BUILD_DOCUMENTATION "Create and install the HTML based API documentation (requires Doxygen)" ${DOXYGEN_FOUND})
 
-if (BUILD_DOCUMENTATION)
-  if (NOT DOXYGEN_FOUND)
+if(BUILD_DOCUMENTATION)
+  if(NOT DOXYGEN_FOUND)
     add_custom_target(doc
       COMMAND echo "You must have doxygen installed at time of configure"
       VERBATIM)
-  else (NOT DOXYGEN_FOUND)
-    set (doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/doxyfile.in)
-    set (doxyfile ${CMAKE_CURRENT_BINARY_DIR}/doxyfile)
+  else(NOT DOXYGEN_FOUND)
+    set(doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/doxyfile.in)
+    set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/doxyfile)
 
     configure_file (${doxyfile_in} ${doxyfile} @ONLY)
     add_custom_target(doc
@@ -36,6 +36,6 @@ if (BUILD_DOCUMENTATION)
       DEPENDS ${doxyfile_in}
       VERBATIM)
 
-    install (DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/doc/${PROJECT_NAME})
-  endif (NOT DOXYGEN_FOUND)
-endif (BUILD_DOCUMENTATION)
+    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/doc/${PROJECT_NAME})
+  endif(NOT DOXYGEN_FOUND)
+endif(BUILD_DOCUMENTATION)
