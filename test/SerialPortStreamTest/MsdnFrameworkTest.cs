9848588460c7cc749304eeecd2c5cbb6a17d5241
Jason Curl <jcurl@arcor.de>
2016-04-28 12:01:09 +0200

    Remove outdated test code.

--- test/SerialPortStreamTest/MsdnFrameworkTest.cs
+++ test/SerialPortStreamTest/MsdnFrameworkTest.cs
@@ -1,9 +1,11 @@
-﻿namespace RJCP.IO.Ports.SerialPortStreamTest
+﻿namespace RJCP.IO.Ports.FrameworkTest
 {
     using System;
     using System.IO.Ports;
+    using System.Text;
     using System.Threading;
     using NUnit.Framework;
+    using SerialPortStreamTest;
 
     [TestFixture]
     public class MsdnFrameworkTest
@@ -93,6 +95,23 @@
                     Console.WriteLine("{0}", bufferCount);
                 }
             }
+        }
+
+        [Test]
+        public void DecoderTooManyBytes()
+        {
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            Decoder decoder = encoding.GetDecoder();
+
+            byte[] data = new byte[] { 0x61, 0xE2, 0x82, 0xAC, 0x40, 0x41 };
+            char[] oneChar = new char[2];
+
+            int bu;
+            int cu;
+            bool complete;
+            decoder.Convert(data, 0, 2, oneChar, 0, 1, false, out bu, out cu, out complete);
+            Assert.That(bu, Is.EqualTo(1));
+            Assert.That(cu, Is.EqualTo(1));
         }
     }
 }
