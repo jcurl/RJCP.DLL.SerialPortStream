a409c26a7700e694eefcec5bf3682099b5465da4
Jason Curl <jcurl@arcor.de>
2016-10-13 21:52:48 +0200

    Update to version 2.0.2

--- code/Properties/AssemblyInfo.cs
+++ code/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RJCP")]
 [assembly: AssemblyProduct("SerialPortStream")]
-[assembly: AssemblyCopyright("Copyright © Jason Curl 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © Jason Curl 2012-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -43,5 +43,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
