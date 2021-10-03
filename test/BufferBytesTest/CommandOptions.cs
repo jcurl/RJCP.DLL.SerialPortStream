f9bbf444e307beab093e76661275418dd10c982e
Jason Curl <jcurl@arcor.de>
2021-10-03 16:15:50 +0200

    SerialPortStream: Add an integration test that checks BytesToWrite
    When reviewing code, it was observed that when writing, the buffer for the number of bytes to write is only decremented when the write is returned. If the overlapped operation only completed when the actual write was complete, this could result in BytesToWrite counting the same bytes being sent twice, thus providing a value higher than what reality is.
    
    Tested on various chipsets, and confirmed that when writing, the BytesToWrite would return more data than what is actually written.
    * PL2303 RA
    * CP2101
    
    Issue: DOTNET-425

---
+++ test/BufferBytesTest/CommandOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace RJCP.IO.Ports
+{
+    using RJCP.Core.CommandLine;
+
+    internal class CommandOptions
+    {
+        [Option('p', "port", true)]
+        public string Port { get; private set; }
+
+        [Option('b', "baud")]
+        public int Baud { get; private set; } = 9600;
+
+        [Option('l', "length")]
+        public int Length { get; private set; } = 10240;
+    }
+}
---
+++ test/BufferBytesTest/Program.cs
@@ -0,0 +1,66 @@
+// Copyright © Jason Curl 2012-2021
+// Sources at https://github.com/jcurl/SerialPortStream
+// Licensed under the Microsoft Public License (Ms-PL)
+
+namespace RJCP.IO.Ports
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using RJCP.Core.CommandLine;
+
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            CommandOptions options = new CommandOptions();
+            try {
+                Options.Parse(options, args);
+            } catch (OptionException ex) {
+                Console.WriteLine($"{ex.Message}");
+                return 1;
+            }
+
+            byte[] buffer = new byte[2048];
+            Random r = new Random();
+            r.NextBytes(buffer);
+
+            try {
+                Console.WriteLine($"Testing port {options.Port} at Baud {options.Baud}");
+                using (SerialPortStream port = new SerialPortStream(options.Port, options.Baud)) {
+                    port.Open();
+
+                    // Monitor the number of bytes to write and print when it changes. On Windows, it depends on the
+                    // internal buffer and the number of bytes to write provided by the driver.
+                    Task monitor = new TaskFactory().StartNew(() => {
+                        int prevBytesToWrite = -1;
+                        int bytesToWrite;
+                        while (port.IsOpen) {
+                            bytesToWrite = port.BytesToWrite;
+                            if (bytesToWrite != prevBytesToWrite) {
+                                Console.WriteLine($"BytesToWrite = {bytesToWrite}");
+                                prevBytesToWrite = bytesToWrite;
+                            }
+                        }
+                    });
+
+                    Thread.Sleep(50);
+                    int write = options.Length;
+                    int written = 0;
+                    while (write > 0) {
+                        int writeLength = write > buffer.Length ? buffer.Length : write;
+                        written += writeLength;
+                        Console.WriteLine($"Writing: {writeLength} (write {written} bytes of {options.Length})");
+                        port.Write(buffer, 0, writeLength);
+                        write -= writeLength;
+                    }
+                    Console.WriteLine("Flushing");
+                    port.Flush();
+                }
+            } catch (Exception ex) {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            return 0;
+        }
+    }
+}
