b5b607183929952625852395458928ff6c057b54
Jason Curl <jcurl@arcor.de>
2024-02-08 21:53:52 +0100

    SerialPortStream: Update InternalApplicationException for .NET Framework
    Fixed SYSLIB0003 (.NET 6.0) and potentially SYSLIB0051 (.NET 8.0), by removing all support for exception serialisation for .NET Core. It is kept for .NET Framework for compatibility.
    
    Issue: DOTNET-943, DOTNET-951

--- code/InternalApplicationException.cs
+++ code/InternalApplicationException.cs
@@ -1,6 +1,9 @@
 ﻿namespace RJCP.IO.Ports
 {
     using System;
+#if NETFRAMEWORK
+    using System.Runtime.Serialization;
+#endif
 
     /// <summary>
     /// Indicates an unexpected internal error occurred in the application.
@@ -9,6 +12,9 @@
     /// The InternalApplicationError indicates a programming error in the application. Such an
     /// error should never occur and always indicates a bug in the application itself.
     /// </remarks>
+#if NETFRAMEWORK
+    [Serializable]
+#endif
     internal class InternalApplicationException : Exception
     {
         /// <summary>
@@ -31,5 +37,19 @@
         /// Visual Basic) if no inner exception is specified.
         /// </param>
         public InternalApplicationException(string message, Exception innerException) : base(message, innerException) { }
+
+#if NETFRAMEWORK
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InternalApplicationException"/> class.
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="StreamingContext"/> that contains contextual information about the source or destination.
+        /// </param>
+        protected InternalApplicationException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+#endif
     }
 }
