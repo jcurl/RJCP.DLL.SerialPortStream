2eaca9974c0dc2abbc0b5c2dff3d99c37271492b
Jason Curl <jcurl@arcor.de>
2024-02-11 21:20:00 +0100

    SerialPortStream: Refactor to remove empty method
    There is a Microsoft implementation of System.IO.Ports.SerialPort, but this is intended only for Windows. It doesn't make sense to have such an implementation for Linux. Use conditional includes to reduce code on Linux.
    
    Issue: DOTNET-947

--- code/Serial/UnixNativeSerial.cs
+++ code/Serial/UnixNativeSerial.cs
@@ -179,7 +179,15 @@ namespace RJCP.IO.Ports.Serial
                 // libnserial is version < 1.1.0
                 ports = null;
             }
-            if (ports == null) return System.IO.Ports.SerialPort.GetPortNames();
+
+            if (ports == null)
+#if NETFRAMEWORK
+                // The implementation, most likely from Mono.
+                return System.IO.Ports.SerialPort.GetPortNames();
+#else
+                // There is no alternative implementation for Linux.
+                return Array.Empty<string>();
+#endif
 
             string[] portNames = new string[ports.Length];
             for (int i = 0; i < ports.Length; i++) {
@@ -205,20 +213,21 @@ namespace RJCP.IO.Ports.Serial
             } catch (EntryPointNotFoundException) {
                 ports = null;
             }
-            if (ports == null) return GetRuntimePortDescriptions();
+            if (ports != null)
+                return ports;
 
-            return ports;
-        }
+#if NETFRAMEWORK
+            string[] portNamess =
+                System.IO.Ports.SerialPort.GetPortNames();
+            PortDescription[] portdescs = new PortDescription[portNamess.Length];
 
-        private static PortDescription[] GetRuntimePortDescriptions()
-        {
-            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
-            PortDescription[] portdescs = new PortDescription[ports.Length];
-
-            for (int i = 0; i < ports.Length; i++) {
-                portdescs[i] = new PortDescription(ports[i], "");
+            for (int i = 0; i < portNamess.Length; i++) {
+                portdescs[i] = new PortDescription(portNamess[i], "");
             }
             return portdescs;
+#else
+            return Array.Empty<PortDescription>();
+#endif
         }
 
         /// <summary>
